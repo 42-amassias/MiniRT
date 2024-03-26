f385250575a0d1c7a01a6aaf19d2731a6ffe7e4c
amassias <amassias@student.42lehavre.fr>
2024-03-26 14:54:19 +0100

    Fix: The float parser was not giving the correct sign to the result if there was no `.` in the string representation.

--- src/scene/loader/parser_float.c
+++ src/scene/loader/parser_float.c
@@ -6,7 +6,7 @@
 /*   By: amassias <amassias@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:47:53 by amassias          #+#    #+#             */
-/*   Updated: 2024/03/24 12:06:07 by amassias         ###   ########.fr       */
+/*   Updated: 2024/03/26 14:52:16 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,19 +39,19 @@ char	*parser_get_float(
 			float *v
 			)
 {
-	bool		has_int;
-	bool		has_dec;
-	const float	sign = 1.f;
-	float		dec_part;
-	size_t		i;
+	bool	has_int;
+	bool	has_dec;
+	float	sign;
+	float	dec_part;
+	size_t	i;
 
-	((void)0, has_int = false, i = 0, *v = 0.f, dec_part = 0.f);
+	((void)0, has_int = false, sign = 1.f, i = 0, *v = 0.f, dec_part = 0.f);
 	if (*str == '+' || *str == '-')
-		((void)0, *((float *)&sign) = -1.f, ++str);
+		((void)0, sign = -1.f, ++str);
 	while (ft_isdigit(*str))
 		((void)0, has_int = true, *v = 10.f * *v + (float)(*str++ - '0'));
 	if (*str++ != '.')
-		return (str - 1);
+		return (*v *= sign, str - 1);
 	while (ft_isdigit(*str))
 	{
 		((void)0, has_dec = true, ++i);
